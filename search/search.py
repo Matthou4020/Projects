f62211d7ab9580328ca5311d6bcc866a483c11e3
Matthieu <martens.matthieu@hotmail.com>
2024-03-14 00:53:03 +0100

    frontend

--- search/search.py
+++ search/search.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
  
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+
+@app.route("/image_search", methods=["GET", "POST"])
+def image_search():
+    if request.method == "GET":
+        return render_template("image_search.html")
+
+@app.route("/advanced_search", methods=["GET", "POST"])
+def advanced_search():
+    if request.method == "GET":
+        return render_template("advanced_search.html")
+
+
+
