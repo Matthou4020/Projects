e6f91503d8fbb5c1a86f193544c79f338b3fc8cb
Matthieu <martens.matthieu@hotmail.com>
2024-03-21 22:39:22 +0100

    feature: Add new path 'new_listing'
    Allow users to create a new listing

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListing(models.Model):
+    AuctionName = models.CharField(max_length=50)
+    HighestBidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="highestbidder")
+
+class Bids(models.Model):
+    BidAmount = models.IntegerField(max_length = 9)
+
+class Comments(models.Model):
+    Comment = models.TextField(max_length=200)
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,15 +1,15 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
+from .models import *
 from .models import User
 
 
 def index(request):
     return render(request, "auctions/index.html")
-
 
 def login_view(request):
     if request.method == "POST":
@@ -61,3 +61,7 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+@login_required
+def new_listing(request):
+    return render(request, "auction/new_listing.html")
