f04a6d0acd835f61b8ebb9c9d4bfd1434a696b8b
Matthieu <martens.matthieu@hotmail.com>
2024-04-03 23:50:09 +0200

    feat: Add archive and restore mail button
    Users can add mails to the archive when reading them individually. They also can
    restore them from the archive view.

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -39,6 +39,8 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#display-mail').style.display = 'none';
+  document.querySelector('#archives').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -60,20 +62,30 @@ function display_mail(mail) {
   if (document.querySelector('#header')) {
   header.innerHTML = `Subject: ${subject}. ${sender}${timestamp}`;
   paragraph.innerHTML = `${body}`;
-}
-else {
-  const header = document.createElement('h4');
-  header.innerHTML = `Subject: ${subject}. ${sender}${timestamp}`;
-  header.id = "header"
-  const paragraph = document.createElement('p');
-  paragraph.innerHTML = `${body}`;
-  emails_view.append(header, paragraph);
-}
+  }
+  else {
+    const header = document.createElement('h4');
+    header.innerHTML = `Subject: ${subject}. ${sender}${timestamp}`;
+    header.id = "header"
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = `${body}`;
+    emails_view.append(header, paragraph);
+  }
+  const archive = document.createElement('button');
+  archive.className = "btn btn-sm btn-outline-secondary";
+  archive.id = "archive";
+  archive.innerHTML = "Archive";
+  emails_view.append(archive);
 
-  
-
-  
-
+  archive.addEventListener('click', () => {
+    fetch(`/emails/${mail.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        archived: true
+  })
+});
+load_mailbox('inbox')
+});
 }
 
 function load_mailbox(mailbox) {
@@ -82,71 +94,87 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#display-mail').style.display = 'none'
+  document.querySelector('#archives').style.display = 'none'
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
   
   if (mailbox === 'archive') {
+    document.querySelector('#archives').style.display = 'block'
     fetch('/emails/archive')
     .then(response => response.json())
     .then(emails => {
-    console.log(emails);
-    })
-  }
+      if (!document.querySelector('#restore')) {
+    for(let i = 0; i < emails.length; i++) {
+      const subject = emails[i].subject;
+      const sender = emails[i].sender;
+      const timestamp = emails[i].timestamp;
+      const element = document.createElement('div');
+      element.innerHTML = `${timestamp} Subject: ${subject}. From: ${sender}`
+      
+      const archived = document.querySelector('#archives')
+      archives.append(element);
+      const restore = document.createElement('button');
+      restore.className = "btn btn-sm btn-outline-secondary";
+      restore.id = "restore";
+      restore.innerHTML = "Restore";
+      archives.append(restore);
+      restore.addEventListener('click', () => {
+        fetch(`/emails/${emails[i].id}`, {
+          method: 'PUT',
+          body: JSON.stringify({
+            archived: false
+          })
+        });
+        load_mailbox('inbox')
+      });
+    }}
+  })
+}
 
   if (mailbox === 'inbox') {
     fetch('/emails/inbox')
     .then(response => response.json())
     .then(emails => {
-      for (let i = 0; i < emails.length; i++) {
-        const subject = emails[i].subject;
-        const sender = emails[i].sender;
-        const timestamp = emails[i].timestamp;
-        const read = emails[i].read;
-        const id = emails[i].id;
-        const element = document.createElement('div');
-        element.id = id
-        element.innerHTML = `${timestamp} Subject: ${subject}. From: ${sender}`
-        if (read) {
-          element.style.backgroundColor = 'lightgray';
-        }
-        const emailsview = document.querySelector('#emails-view')
-        emailsview.append(element);
-        
-        element.addEventListener('click', function() {
-          fetch(`/emails/${element.id}`)
-          .then(response => response.json())
-          .then(email => {
-            element.style.backgroundColor = 'white'
-            display_mail(email)
+        for (let i = 0; i < emails.length; i++) {
+          const subject = emails[i].subject;
+          const sender = emails[i].sender;
+          const timestamp = emails[i].timestamp;
+          const read = emails[i].read;
+          const id = emails[i].id;
+          const element = document.createElement('div');
+          element.id = id
+          element.innerHTML = `${timestamp} Subject: ${subject}. From: ${sender}`
+          if (read) {
+            element.style.backgroundColor = 'lightgray';
+          }
+          const emailsview = document.querySelector('#emails-view')
+          emailsview.append(element);
+          
+          element.addEventListener('click', function() {
+            fetch(`/emails/${element.id}`)
+            .then(response => response.json())
+            .then(email => {
+              element.style.backgroundColor = 'white'
+              display_mail(email)
 
-            fetch(`/emails/${email.id}`, {
-              method: 'PUT',
-              body: JSON.stringify({
-                  read: true
+              fetch(`/emails/${email.id}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    read: true
+                })
               })
+            }
+           ) })
+          }}
+           )}
+           if (mailbox === 'sent') {
+            fetch('/emails/sent')
+            .then(response => response.json())
+            .then(emails => {
+            console.log(emails);
             })
-          })
-        }
-      )}
-  });
-  }
-  //{
-    //"id": 100,
-    //"sender": "foo@example.com",
-    //"recipients": ["bar@example.com"],
-    //"subject": "Hello!",
-    //"body": "Hello, world!",
-    //"timestamp": "Jan 2 2020, 12:00 AM",
-    //"read": false,
-    //"archived": false
-//}
+          };
+        
+      }
 
-  if (mailbox === 'sent') {
-    fetch('/emails/sent')
-    .then(response => response.json())
-    .then(emails => {
-    console.log(emails);
-    })
-  }
-};
